d527b7492cd6bbdd97581f479e4a45c1146418f3
Yangholmes <yangholmes@126.com>
2017-07-07 17:17:34 +0800

    init project. finish picker framework, doing a datetimepicker base on jQuery.

---
+++ datetimepicker.js
@@ -0,0 +1,46 @@
+/* jshint esversion: 6 */
+/**
+ * Yangholmes 2017-07-07
+ * @param  {[type]} initDt [description]
+ * @param  {[type]} config [description]
+ * @return {[type]}        [description]
+ */
+var Datetimepicker = function (config) {
+  this.initDateTimePicker(config);
+};
+Datetimepicker.prototype = {
+  year, month, day, hour, minute,
+
+  /**
+   * [constructor]
+   * @type {[type]}
+   */
+  constructor: datetimepicker,
+
+  /**
+   * [initDateTimePicker description]
+   * @param  {[type]} initDt [description]
+   * @param  {[type]} config [description]
+   * @return {[type]}        [description]
+   */
+  initDateTimePicker: (config) => {
+
+  },
+
+  show: () => {
+
+  },
+
+  close: () => {
+
+  },
+
+  render: () => {
+
+  },
+
+  select: () => {
+    
+  }
+
+};
